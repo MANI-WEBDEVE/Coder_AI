3cc471e0c7f4560c0454da4327098e6661491c49
MANI-WEBDEVE <nudmaufon@gmail.com>
2025-02-12 05:38:05 +0500

    fixed UI

--- src/app/(main)/workspace/[id]/page.tsx
+++ src/app/(main)/workspace/[id]/page.tsx
@@ -5,11 +5,11 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='p-2 pr-8 w-full h-full'>
+    <div className='p-2 md:pr-8 w-full h-full flex '>
       
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-10 w-full h-full'>
+        <div className='grid grid-cols-1 md:grid-cols-3 items-center justify-center md:gap-10 w-full h-full'>
             <ChatView/>
-            <div className='col-span-2 w-full h-full'>
+            <div className='col-span-2 w-full h-full mt-10'>
                 <CodeView/>
             </div>
         </div>
